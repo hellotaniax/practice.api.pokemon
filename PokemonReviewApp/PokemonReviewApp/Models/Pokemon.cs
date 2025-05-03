147f84f81a7fe4c6685325fb701acab6a9d28558
TaniaDevCode <gg13385397@gmail.com>
2025-05-02 23:00:46 -0500

    Listo

---
+++ PokemonReviewApp/PokemonReviewApp/Models/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ICollection<Review> Reviews { get; set; } // un Pokemon puede tener múltiples Review asociados
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
+
+    }
+}
